4fbdb47460db7e6707d55a3ba9ce2f54687477b7
alf1e <alfiewbarlow@gmail.com>
2022-08-07 00:20:35 +0100

    Fix pls

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ecs-license-api',
-    packages=['elicense'],
+    packages=['elicense', 'elicense.v1'],
     include_package_data=True,
     install_requires=[
         'flask',
